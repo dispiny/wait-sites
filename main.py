ebb9f927b9bbe50a593e91a63a18626a24c35c76
Jongmin Park <60399075+dispiny@users.noreply.github.com>
2023-08-26 11:16:30 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import threading
 import time
 import random
+import os
 
 app = Flask(__name__)
+video_folder = "static"
+video_files = [os.path.join(video_folder, filename) for filename in os.listdir(video_folder) if filename.endswith(".mp4")]
 
-# 최대 동시 요청 수
 MAX_CONCURRENT_REQUESTS = 5
 
-# 현재 처리 중인 요청 수를 추적하기 위한 변수
 current_requests = 0
-
-# 뮤텍스를 사용하여 현재 요청 수를 동기화
 mutex = threading.Lock()
 
 @app.route('/healthcheck')
 def health():
     return {"msg": "healthy"}
 
+
 @app.route('/web')
 def st():
-    return {"ss": "ㅅㄷㄴㅅ"}
+    random_video_filename = random.choice(video_files)
+    print(random_video_filename)
+    random_video_filename = random_video_filename.replace("static\\", "")
+    return render_template('video.html', video_url=random_video_filename)
 
 @app.route('/process')
 def index():
     global current_requests
 
-    # 요청이 들어올 때마다 현재 요청 수를 증가시킴
     with mutex:
         current_requests += 1
 
     if current_requests <= MAX_CONCURRENT_REQUESTS:
-        # 최대 동시 요청 수를 초과하지 않으면 처리
-        # 5~15초 동안 대기
         processing_time = random.randint(5, 15)
         time.sleep(processing_time)
         
@@ -41,10 +41,9 @@ def index():
         return jsonify({'message': 'Request processed in {} seconds'.format(processing_time)})
 
     else:
-        # 최대 동시 요청 수를 초과한 경우 503 오류 반환
         with mutex:
             current_requests -= 1
         return jsonify({'message': 'errrrrrr'}), 503
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
